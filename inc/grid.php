981863175fc86a71a88d3582a0797394f7bfaad6
craftwerkberlin <51531217+craftwerkberlin@users.noreply.github.com>
2021-02-02 12:55:43 +0100

    Add files via upload

---
+++ inc/grid.php
@@ -0,0 +1,83 @@
+<?php
+/*Post/Page Grid Shortcode 
+Posts: [bs-post-grid type="post" category="documentation, category-default" order="ASC" orderby="title" posts="6"]
+Pages: [bs-post-grid type="page" post_parent="413" order="ASC" orderby="title" posts="6"]
+*/
+
+
+// Post Grid Shortcode
+add_shortcode( 'bs-post-grid', 'bootscore_post_grid' );
+function bootscore_post_grid( $atts ) {
+	ob_start();
+	extract( shortcode_atts( array (
+		'type' => 'post',
+		'order' => 'date',
+		'orderby' => 'date',
+		'posts' => -1,
+		'category' => '',
+        'post_parent'    => '',
+        
+	), $atts ) );
+	$options = array(
+		'post_type' => $type,
+		'order' => $order,
+		'orderby' => $orderby,
+		'posts_per_page' => $posts,
+		'category_name' => $category,
+        'post_parent' => $post_parent,
+        
+	);
+	$query = new WP_Query( $options );
+	if ( $query->have_posts() ) { ?>
+
+
+<div class="row">
+    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+    <div class="col-md-6 col-lg-4 col-xxl-3 mb-4">
+        <div class="card h-100">
+            <!-- Featured Image-->
+            <?php the_post_thumbnail('medium', array('class' => 'card-img-top')); ?>
+
+            <div class="card-body d-flex flex-column">
+                
+                <?php bootscore_category_badge(); ?>
+                
+                <!-- Title -->
+                <h2 class="blog-post-title">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h2>
+                <!-- Meta -->
+                <?php if ( 'post' === get_post_type() ) : ?>
+                <small class="text-muted mb-2">
+                    <?php
+				        bootscore_date();
+				        bootscore_author();
+				        bootscore_comments();
+				        bootscore_edit();
+				    ?>
+                </small>
+                <?php endif; ?>
+                <!-- Excerpt & Read more -->
+                <div class="card-text">
+                    <?php the_excerpt(); ?>
+                </div>
+                            
+                <div class="mt-auto">
+                    <a class="read-more" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'bootscore'); ?></a>
+                </div>
+                <!-- Tags -->
+                <?php bootscore_tags(); ?>
+            </div>
+        </div>
+    </div>
+    <?php endwhile; wp_reset_postdata(); ?>
+</div>
+
+<?php $myvariable = ob_get_clean();
+	return $myvariable;
+	}	
+}
+
+// Post Grid Shortcode End
---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+/*Plugin Name: bS5 Post / Page Grid / List 5
+Plugin URI: https://bootscore.me/plugins/bs-post-page-grid-list/
+Description: Displays posts from category or child pages from parent id in your post or page via shortcode. Post Grid [bs-post-grid type="post" category="documentation, category-default" order="ASC" orderby="title" posts="6"], Post List [bs-post-list type="post" category="documentation, category-default" order="DESC" orderby="date" posts="6"], Child Page Grid [bs-post-grid type="page" post_parent="413" order="ASC" orderby="title" posts="6"], Child Page List [bs-post-list type="page" post_parent="413" order="DESC" orderby="date"]
+Version: 5.0.0
+Author: Bastian Kreiter
+Author URI: https://crftwrk.de
+License: GPLv2
+*/
+
+
+
+// Include Scripts
+include_once('inc/grid.php');
+include_once('inc/list.php');

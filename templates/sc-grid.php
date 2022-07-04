910f24f0bb4502767b943a0a6218e066ed80a5d7
craftwerk <pst@crftwrk.de>
2022-07-04 10:38:14 +0200

    Reformat templates

--- templates/sc-grid.php
+++ templates/sc-grid.php
@@ -4,8 +4,8 @@
  *
  * This template can be overriden by copying this file to your-theme/bs-grid-main/sc-grid.php
  *
- * @author 		bootScore
- * @package 	bS Grid
+ * @author 	  bootScore
+ * @package   bS Grid
  * @version   5.2.1.0
 
 Post/Page/CPT Grid Shortcodes
--- templates/sc-hero.php
+++ templates/sc-hero.php
@@ -4,8 +4,8 @@
  *
  * This template can be overriden by copying this file to your-theme/bs-grid-main/sc-hero.php
  *
- * @author 		bootScore
- * @package 	bS Grid
+ * @author 	  bootScore
+ * @package   bS Grid
  * @version   5.2.1.0
 
 Post/Page/CPT Hero Grid Shortcodes

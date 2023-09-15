0aa0fe47df6ee2ddf12828940dfc29e50ffd5673
craftwerk <pst@crftwrk.de>
2023-09-15 12:14:00 +0200

    Update bs-grid.js

--- js/bs-grid.js
+++ js/bs-grid.js
@@ -1,3 +1,47 @@
+
+// Accordion
+document.querySelectorAll('.bs-accordion').forEach(function (accordion, i) {
+  // Add dynamic id's to accordion
+  accordion.querySelector(".accordion").id = "accordion_" + i;
+  // Add dynamic data-bs-parent to accordion-collapse
+  accordion.querySelector('.accordion-collapse').setAttribute("data-bs-parent", "#accordion_" + i);
+});
+
+document.querySelectorAll('.bs-accordion .accordion-item').forEach(function (item, i) {
+  // Add dynamic id's to accordion-header
+  item.querySelector(".accordion-header").id = "heading_" + i;
+  // Add dynamic data-bs-target and aria-controls to accordion-button
+  item.querySelector(".accordion-button").setAttribute("data-bs-target", "#accordion_collapse_" + i);
+  item.querySelector(".accordion-button").setAttribute("aria-controls", "accordion_collapse_" + i);
+  // Add dynamic id and aria-labelledby to accordion-collapse
+  item.querySelector(".accordion-collapse").id = "accordion_collapse_" + i;
+  item.querySelector(".accordion-collapse").setAttribute("aria-labelledby", "heading_" + i);
+});
+
+
+// Tabs  
+document.querySelectorAll('.bs-tabs .nav.nav-tabs .nav-link').forEach(function (link, i) {
+  // Add dynamic data-bs-target and aria-controls to nav-tabs
+  link.setAttribute("data-bs-target", "#tab_content_" + i);
+  link.setAttribute("aria-controls", "tab_content_" + i);
+});
+
+document.querySelectorAll('.bs-tabs .tab-content .tab-pane').forEach(function (pane, i) {
+  // Add dynamic id and aria-labelledby to tab-content
+  pane.id = "tab_content_" + i;
+  pane.setAttribute("aria-labelledby", "tab_content_" + i);
+});
+
+// First item active
+document.querySelector('.bs-tabs .nav-link:first-child').classList.add('active');
+document.querySelector('.bs-tabs .tab-content .tab-pane:first-child').classList.add('show', 'active');
+
+
+
+
+
+/*
+
 jQuery(function ($) {
 
   // Accordion
@@ -31,6 +75,7 @@ jQuery(function ($) {
 
   // First item active
   $('.bs-tabs .nav-link:first-child').addClass('active');
-  $('.bs-tabs .tab-content .tab-pane:first-child').addClass('show active')
+  $('.bs-tabs .tab-content .tab-pane:first-child').addClass('show active');
 
 }); // jQuery End
+*/

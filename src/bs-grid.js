84f4aa1076c382f4f35320de07eb87d0a1a5e9cd
craftwerk <pst@crftwrk.de>
2023-09-15 18:30:00 +0200

    Minify script and add source

--- src/bs-grid.js
+++ src/bs-grid.js
@@ -1,3 +1,10 @@
+/*!
+ * bS Grid
+ * 
+ * @version 5.4.0
+ */
+
+
 // Accordion
 document.querySelectorAll('.bs-accordion').forEach(function (accordion, i) {
   // Add dynamic id's to accordion
@@ -17,6 +24,7 @@ document.querySelectorAll('.bs-accordion .accordion-item').forEach(function (ite
   item.querySelector(".accordion-collapse").setAttribute("aria-labelledby", "heading_" + i);
 });
 
+
 // Tabs  
 document.querySelectorAll('.bs-tabs .nav.nav-tabs .nav-link').forEach(function (link, i) {
   // Add dynamic data-bs-target and aria-controls to nav-tabs
@@ -29,19 +37,6 @@ document.querySelectorAll('.bs-tabs .tab-content .tab-pane').forEach(function (p
   pane.id = "tab_content_" + i;
   pane.setAttribute("aria-labelledby", "tab_content_" + i);
 });
-
-/*
-// First item active if it exists
-const firstNavLink = document.querySelector('.bs-tabs .nav-link:first-child');
-if (firstNavLink) {
-  firstNavLink.classList.add('active');
-}
-
-const firstTabPane = document.querySelector('.bs-tabs .tab-content .tab-pane:first-child');
-if (firstTabPane) {
-  firstTabPane.classList.add('show', 'active');
-}
-*/
 
 // First item active if it exists
 // Handle each instance of tabs separately
@@ -57,50 +52,3 @@ document.querySelectorAll('.bs-tabs').forEach(function (tabs) {
     firstTabPane.classList.add('show', 'active');
   }
 });
-
-
-
-
-
-
-
-
-/*
-
-jQuery(function ($) {
-
-  // Accordion
-  $('.bs-accordion').each(function (i, e) {
-    // Add dynamic id's to accordion
-    $(this).find(".accordion").attr("id", "accordion_" + i);
-    // Add dynamic data-bs-parent to accordion-collapse
-    $(this).find('.accordion-collapse').attr("data-bs-parent", "#accordion_" + i);
-  });
-
-  $('.bs-accordion .accordion-item').each(function (i, e) {
-    // Add dynamic id's to accordion-header
-    $(this).find(".accordion-header").attr("id", "heading_" + i);
-    // Add dynamic data-bs-target and aria-controls to accordion-button
-    $(this).find(".accordion-button").attr("data-bs-target", "#accordion_collapse_" + i).attr("aria-controls", "#accordion_collapse_" + i);
-    // Add dynamic id and aria-labelledby to accordion-collapse
-    $(this).find(".accordion-collapse").attr("id", "accordion_collapse_" + i).attr("aria-labelledby", "heading_" + i);
-  });
-
-  
-  // Tabs  
-  $('.bs-tabs .nav.nav-tabs .nav-link').each(function (i, e) {
-    // Add dynamic data-bs-target and aria-controls to nav-tabs
-    $(this).attr("data-bs-target", "#tab_content_" + i).attr("aria-controls", "tab_content_" + i);
-  });
-
-  $('.bs-tabs .tab-content .tab-pane').each(function (i, e) {
-    // Add dynamic id and aria-labelledby to tab-content
-    $(this).attr("id", "tab_content_" + i).attr("aria-labelledby", "tab_content_" + i);
-  });
-
-  // First item active
-  $('.bs-tabs .nav-link:first-child').addClass('active');
-  $('.bs-tabs .tab-content .tab-pane:first-child').addClass('show active');
-
-}); // jQuery End
-*/

75c020754e1b3a058c3cda24b04fb7a365ff1f78
craftwerk <pst@crftwrk.de>
2025-12-16 12:03:02 +0100

    Move locate template into an own file

--- main.php
+++ main.php
@@ -32,128 +32,15 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 $myUpdateChecker->setBranch('main');
 
 
-
+/**
+ * Load required files
+ */
+require_once('inc/locate-template.php');  // Allow template overrides in child-theme
 require_once('inc/enqueue.php');          // Enqueue scripts and styles
 require_once('inc/patterns.php');         // Register patterns
 
 
 
-
-/**
- * Locate template.
- *
- * Locate the called template.
- * Search Order:
- * 1. /themes/theme/bs-grid/$template_name
- * 2. /themes/theme/bs-grid-main/$template_name
- * 3. /themes/theme/$template_name
- * 4. /plugins/bs-grid-main/templates/$template_name.
- *
- * @since 5.6.0
- *
- * @param 	string 	$template_name			Template to load.
- * @param 	string 	$string $template_path	Path to templates.
- * @param 	string	$default_path			Default path to template files.
- * @return 	string 							Path to the template file.
- */
-function bs_grid_locate_template($template_name, $template_path = '', $default_path = '') {
-
-  // Set default plugin templates path.
-  if (!$default_path) :
-    $default_path = plugin_dir_path(__FILE__) . 'templates/'; // Path to the template folder
-  endif;
-
-  // Check if 'bs-grid/' exists in the theme.
-  $bs_grid_path = get_theme_file_path('bs-grid/' . $template_name);
-  if (file_exists($bs_grid_path)) {
-    return $bs_grid_path;
-  }
-
-  // Check if 'bs-grid-main/' exists in the theme.
-  // Fallback for existing 'bs-grid-main/' folders in child theme
-  $bs_grid_main_path = get_theme_file_path('bs-grid-main/' . $template_name);
-  if (file_exists($bs_grid_main_path)) {
-    return $bs_grid_main_path;
-  }
-
-  // If neither 'bs-grid/' nor 'bs-grid-main/' exists, return the default path.
-  return $default_path . $template_name;
-}
-
-
-/**
- * Get template.
- *
- * Search for the template and include the file.
- *
- * @since 1.0.0
- *
- * @see bs_grid_locate_template()
- *
- * @param string 	$template_name			Template to load.
- * @param array 	$args					Args passed for the template file.
- * @param string 	$string $template_path	Path to templates.
- * @param string	$default_path			Default path to template files.
- */
-function bs_grid_get_template($template_name, $args = array(), $tempate_path = '', $default_path = '') {
-
-  if (is_array($args) && isset($args)) :
-    extract($args);
-  endif;
-
-  $template_file = bs_grid_locate_template($template_name, $tempate_path, $default_path);
-
-  if (!file_exists($template_file)) :
-    _doing_it_wrong(__FUNCTION__, sprintf('<code>%s</code> does not exist.', $template_file), '1.0.0');
-    return;
-  endif;
-
-  include $template_file;
-}
-
-
-/**
- * Templates.
- *
- * This func tion will output the templates
- * file from the /templates.
- *
- * @since 1.0.0
- */
-
-function bs_grid() {
-
-  return bs_grid_get_template('sc-grid.php');
-}
-add_action('wp_head', 'bs_grid');
-
-
-function bs_grid_list() {
-
-  return bs_grid_get_template('sc-list.php');
-}
-add_action('wp_head', 'bs_grid_list');
-
-
-function bs_grid_hero() {
-
-  return bs_grid_get_template('sc-hero.php');
-}
-add_action('wp_head', 'bs_grid_hero');
-
-
-function bs_grid_accordion() {
-
-  return bs_grid_get_template('sc-accordion.php');
-}
-add_action('wp_head', 'bs_grid_accordion');
-
-
-function bs_grid_tabs() {
-
-  return bs_grid_get_template('sc-tabs.php');
-}
-add_action('wp_head', 'bs_grid_tabs');
 
 
 
